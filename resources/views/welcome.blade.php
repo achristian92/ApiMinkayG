4fb57956fcd5ab39ba2dc146b5afe4ac67747472
Jose Córdova <jose.fcr13@gmail.com>
2018-06-06 14:36:18 -0500

    estilos welcome
    estilos welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,59 +12,8 @@
 
         <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="../resources/assets/css/estilobienvenida.css" rel="stylesheet" type="text/css">
     </head>
-    <style type="text/css">
-        .presentacion{
-            background: url("../resources/assets/img/imagenfondo.png");
-             background-size: auto;
-             padding-bottom: 25px;
-        }
-
-        h2{
-            color: white;
-            font-weight: bold;
-            letter-spacing: 1.5px;
-            padding-top: 15PX;
-            padding-bottom: 15PX;
-
-        }
-
-        h3{
-            color: white;
-            letter-spacing: 1.5px;
-            font-weight: bold;
-            text-align: justify;
-            padding-right: 20px;
-            padding-left: 20px;
-            padding-top: 30px;
-        }
-
-        img{
-            margin-bottom: 93px
-        }
-
-        .botoningreso{
-           
-            text-align: center;
-            padding-top: 50px;
-            
-        }
-
-        #btningreso{
-            font-weight: bold;
-            font-size: 16px;
-            letter-spacing: 2px;
-        }
-
-        .btn{
-            padding: 5px 20px
-        }
-
-        footer{
-            text-align: center;
-            background-color: #d8d8d8;
-        }
-    </style>
     <body>
         <nav class="navbar navbar-default" style="margin-bottom: -21px">
           <div class="container-fluid">
@@ -81,19 +30,32 @@
         <section class="presentacion">
             <div class="container">
                 <div class="row">
-                                <h2 align="center">CHECKLIST GERENCIA</h2>
-            
 
-                <div class="col-sm-6" align="center">
-                     <img src="../resources/assets/img/modulos.png"  width="450" height="140">
-                    <img src="../resources/assets/img/tablaimagen.png" width="450" height="140">
-                                    
+                                <h1 align="center">CHECKLIST GERENCIA</h1>
+                
+                <div class="col-sm-6">
+                    
                 </div>
-                <div class="col-sm-6" align="center">
+
+
+                <div class="col-sm-6">
 
                     <h3>
-                        Realice sus operaciones con total comodidad y transparencia para la eficiencia de su proceso de supervisión de las agencias
+                        Gestiona tus activos
                     </h3>
+
+                    <h4>
+                        <p class="subtextop" id="primerp">
+                           ¡ Olvídate del papel !
+                        </p>
+                        <p class="descri">
+                            Gestiona los activos de tu organización de manera transparente, óptima y segura.
+                        </p>
+                        <p class="descri">
+                            Ten el control eficiente y eficaz de tu supervisión con seguridad.
+                        </p>
+
+                    </h4>
 
                 <div class="botoningreso">
                         <a href="{{ route('login') }}" class="btn btn-primary" id="btningreso">
@@ -111,8 +73,9 @@
         </section>
 
         <footer>
-        <span> Desarrollador por Minkay</span>
-            
+            <center>
+                <span class="pie"> Desarrollador por Minkay</span>
+            </center>
      
         </footer>
          
