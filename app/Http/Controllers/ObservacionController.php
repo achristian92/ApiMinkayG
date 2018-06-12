64efe7db0ef3eb4068adb7c43df4a4fe77707d29
Alan25 <christian922017@gmail.com>
2018-06-12 11:00:11 -0500

    combio controller observacion

--- app/Http/Controllers/ObservacionController.php
+++ app/Http/Controllers/ObservacionController.php
@@ -19,22 +19,38 @@ class ObservacionController extends Controller
     public function store(Request $request)
     {
 
-        $observaciones = new Observacion($request->all());       
 
-        $ruta_imagen = $request->ruta_imagen;
-        $generaridfotos = date('Y_m_d_H_i_s');
-         $ruta = public_path().'/wsuploads/';
+        $observaciones                  = new Observacion($request->all());      
+        $ruta_imagen                    = $request->ruta_imagen;
+        $generaridfotos                 = date('Y_m_d_H_i_s');        
+        $ruta_imagen_actual             = "wsuploads/$generaridfotos.jpg";        
+        $observaciones->ruta_imagen     = $ruta_imagen_actual;
+        $observaciones->idmodulo        = $this->capturaridmodulo($request->idmodulo);
+        $observaciones->fecha_supe      = date('Y_m_d'); 
+
+        $observaciones->save();
+
+        $ruta = public_path().'/wsuploads/';
         $path = $ruta."$generaridfotos.jpg";
         file_put_contents($path,base64_decode($ruta_imagen));
-
-        $ruta_imagen_actual = "wsuploads/$generaridfotos.jpg";        
-        $observaciones->ruta_imagen = $ruta_imagen_actual;
-        $observaciones->save();
        
         return [
-            'success' => true,
-            'dataobser' => $observaciones           
+            'success'      => true                          
                ];
+    }
+     public function capturaridmodulo($nombre_modulo)
+    {
+          $capidmodulo = Modulo::where('nombre_modulo', $nombre_modulo)->first();
+          if($capidmodulo)
+          {
+            return $capidmodulo->idmodulo;        
+          }else
+          {
+          $capidmodulo = Modulo::create([
+          'nombre_modulo' => $nombre_modulo
+          ]);
+            return $capidmodulo->id;        
+          }
     }
 
    
