5a10ac282e436a1558d51be47802bcc57d39632f
Alan25 <christian922017@gmail.com>
2018-06-13 19:34:44 -0500

    correos completos falta validar

--- app/Http/Controllers/CorreosController.php
+++ app/Http/Controllers/CorreosController.php
@@ -10,27 +10,36 @@ class CorreosController extends Controller
 {
     function sendemail(Request $id){
         //Validar si existe codigo de la supe y id modulo
-    	$condcod_supe = Observacion::where('idmodulo', $id->idmodulo)
-    								 ->where('cod_supe',$id->cod_supe)                                     
-    								 ->first();
-    								 
-      $nombre_agencia = DB::table('agencia__supers')->where('agencia__supers.cod_supe', $id->cod_supe)                                 
-                                                ->join('agencias', 'agencias.idagencia', '=', 'agencia__supers.idagencia')       
-                                                ->select('nombre_agencia') 
-                                                ->get();
+    	$obtenerobservaciones = Observacion::where('idmodulo', $id->idmodulo)
+    								 ->where('cod_supe',$id->cod_supe)
+                     ->select('comentario')   
+                     ->limit(3)                                  
+    								 ->get();
 
-      // $nombre_modulo = DB::table('observacions')->where('observacions.cod_supe', $id->cod_supe)
-      //                                           ->where('observacions.idmodulo', $id->idmodulo)                                   
-      //                                           ->join('modulos', 'modulos.idmodulo', '=', 'observacions.idmodulo')       
-      //                                           ->select('nombre_modulo') 
-      //                                           ->get();                                         
+    	// return $obtenerobservaciones;				
 
-           
-    	if($condcod_supe){
+     
+
+      $agen_modu = DB::table('observacions')->where('observacions.cod_supe', $id->cod_supe)
+                                                ->where('observacions.idmodulo', $id->idmodulo)                                   
+                                                ->join('modulos', 'modulos.idmodulo', '=', 'observacions.idmodulo') 
+                                                ->join('agencia__supers', 'agencia__supers.cod_supe', '=', 'observacions.cod_supe') 
+                                                ->join('agencias', 'agencias.idagencia', '=', 'agencia__supers.idagencia')
+                                                ->select('nombre_agencia','nombre_modulo','observacions.cod_supe','fecha_supe')
+                                                ->limit(1) 
+                                                ->get();                                         
+
+       // return $agen_modu;
+       // return response()->json($agen_modu);
+
+
+    	if($obtenerobservaciones){
             $data = array(
-                "cod_supe"       =>  $id->cod_supe,
-                "nombre_agencia" =>  $nombre_agencia
-                // "nombre_modulo" =>  $nombre_modulo,
+                "num_obser"       => $obtenerobservaciones,
+                "agen_modu"       => $agen_modu,
+                "cod_supe"        => $id->cod_supe,
+
+                
             );
             
              Mail::send('emails.welcome', $data , function ($mensaje){                
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -6,22 +6,24 @@
     <title>Nuevas Observaciones</title>
 </head>
 <body>
-     @foreach($nombre_agencia as $age)
+     @foreach($agen_modu as $age)
     <p>Hola! Se ha reportado nuevas observaciones de la Agencia   {{ $age->nombre_agencia }}.</p>
-     @endforeach
-    <p>Codigo generado</p>
-    <ul>
-        <li>Codigo de la Supervicion : {!! $cod_supe !!} </li>
-       
-        <li>Nombre del modulo : </li>
      
-        <li>Fecha : </li>
-    </ul>
-   {{--  <p>Y esta es la posición reportada:</p>
+    <p>Codigo generado :</p>
     <ul>
-        <li>Latitud:</li>
-        <li>Longitud:</li>
-      
-    </ul> --}}
+        <li>Codigo de la Supervicion : {{ $age->cod_supe }} </li>
+       
+        <li>Nombre del modulo : {{ $age->nombre_modulo }} </li>
+     
+        <li>Fecha : {{ $age->fecha_supe }}</li>
+    </ul>
+    @endforeach
+    <p>Estas son algunas de las observaciones:</p>
+    <ul>
+        @foreach($num_obser as $obser)
+        <li>{{ $obser->comentario }}</li>        
+        @endforeach      
+    </ul>
+    <p>Para ver a mas detalle click en el siguiente link :<a href=" http://192.81.219.5/agencia-observaciones/{!! $cod_supe !!}">Aqui!!</p>
 </body>
 </html>
